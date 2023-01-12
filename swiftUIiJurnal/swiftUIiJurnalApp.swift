50e5aa414f8d9d86411efca268d09bb89ca47161
elina <aelin6277@gmail.com>
2023-01-12 01:19:12 +0100

    Initial Commit

---
+++ swiftUIiJurnal/swiftUIiJurnalApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIiJurnalApp.swift
+//  swiftUIiJurnal
+//
+//  Created by Elin A on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIiJurnalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
